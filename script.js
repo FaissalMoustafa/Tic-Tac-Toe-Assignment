a95d3303ecfde0b9c516ba13b147cafd2a397cab
Faissal Moustafa <faissalmoustafa@gmail.com>
2023-07-17 17:11:59 +0300

    fixing the gameplay

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {   //to wait for HTML document to be loaded and parsed
   const sqr_slot = document.querySelectorAll('.sqr-slot');
+  const slotsArray=Array.from(sqr_slot);
   const player_1_input = document.getElementById('1st_player');
   const player_2_input = document.getElementById('2nd_player');
   const start_button = document.getElementById('start');
   const player_1_display = document.getElementById('player-1-display');
   const player_2_display = document.getElementById('player-2-display');
+  const player_1_name = 'Player 1';
+  const player_2_name = 'Player 2';
   let current_player = 'X';
   let player_1_score = 0;
   let player_2_score = 0;
 
-  start_button.addEventListener('click', startResetGame);
+  start_button.addEventListener('click', resetGame);
   
-  function startResetGame() {
-    // Gets players names from input fields
-    const player_1_name = player_1_input.value|| 'Player 1';
-    const player_2_name = player_2_input.value|| 'Player 2';
-
+  function newRound() {
     // Updates players names on display
     player_1_display.textContent = `${player_1_name}: ${player_1_score}`;
     player_2_display.textContent = `${player_2_name}: ${player_2_score}`;
@@ -28,11 +27,40 @@ document.addEventListener('DOMContentLoaded', () => {   //to wait for HTML docum
     // Resets the current player
     current_player = 'X';
   }
+  
+  function resetGame() {
+    // Gets players names from input fields
+    const player_1_name = player_1_input.value|| 'Player 1';
+    const player_2_name = player_2_input.value|| 'Player 2';
+    
+    player_1_score = 0;
+    player_2_score = 0;
+    // Updates players names on display
+    player_1_display.textContent = `${player_1_name}: ${player_1_score}`;
+    player_2_display.textContent = `${player_2_name}: ${player_2_score}`;
+
+    // Resets the game board
+    sqr_slot.forEach(sqr_slot => {
+      sqr_slot.textContent = '';
+    });
+
+    current_player = 'X';
+  };
 
   sqr_slot.forEach(sqr_slot => {
       sqr_slot.addEventListener('click', clickInstance);
   });
-
+  
+  function checkFull(){
+    let full=true;
+    for (let i = 0; i < slotsArray.length; i++) {
+      const element = slotsArray[i];
+      if (element.textContent===''){
+        full=false;
+      }
+    }
+    return full
+  }
   function clickInstance(e) {
     const clicked_slot = e.target;
         // Checks if the cell is empty before placing a mark
@@ -43,17 +71,14 @@ document.addEventListener('DOMContentLoaded', () => {   //to wait for HTML docum
             // Increment the score for the winning player
             if (current_player === 'X') {
               player_1_score++;
-              return startResetGame()
+              // player_1_display.textContent = `${player_1_name}: ${player_1_score}`;
+              return newRound()
             } else {
               player_2_score++;
-              return startResetGame()
+              // player_2_display.textContent = `${player_2_name}: ${player_2_score}`;
+              return newRound()
             }
-    
-            // Update the score display
-           player_1_display.textContent = `${player_1_name}: ${player_1_score}`;
-           player_2_display.textContent = `${player_2_name}: ${player_2_score}`;
-          return;
-        }
+          }
         // Toggles between X and O for the next turn
         if (current_player === 'X') {
           current_player = 'O';
@@ -61,7 +86,10 @@ document.addEventListener('DOMContentLoaded', () => {   //to wait for HTML docum
           current_player = 'X';
         }
       }
-    }
+      if (checkFull()){
+        newRound()
+      }
+    };
   function checkWin(player) {
   for (let i = 0; i < 3; i++) {
     if (
